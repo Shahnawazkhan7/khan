10ea8288ba15b570348e573130a29422838b26ee
devloper <skshahnawazkhan7@gmail.com>
2021-09-25 00:18:32 -0400

    new update

--- sample.java
+++ sample.java
@@ -1,7 +1,7 @@
 class Simple{
 public static void main(String args[]){
 
-System.out.println("Welcome to cloud Knowledge khan shaha")
+System.out.println("Welcome to cloud Knowledge asha shaha")
 }
 }
 
